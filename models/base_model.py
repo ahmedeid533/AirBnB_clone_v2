48d42b9b3a41a4fbd56f6d2b70d9fd3025b90be8
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-20 17:53:56 +0200

    Ahmed Eid 2023/11/20 v0.00B

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
+
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
 import models
+import uuid
+
+Base = declarative_base()
+
 
 class BaseModel:
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
@@ -18,8 +30,6 @@ class BaseModel:
                     self.__dict__[key] = datetime.strptime(val, timeformat)
                 else:
                     self.__dict__[key] = val
-        else:
-            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -28,16 +38,22 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
+
+    def delete(self):
+        """delete from storage"""
+        models.storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = str(type(self).__name__)
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop("_sa_instance_state", None)
         return dictionary
+    
+ 	
