12df56174b5c8ad8f9a6d137413d13a43b676b2f
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-20 16:25:08 +0200

    Ahmed Eid 2023/11/20 v0.005

--- models/base_model.py
+++ models/base_model.py
@@ -13,6 +13,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         elif 'id' not in kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -25,7 +26,6 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
-        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
