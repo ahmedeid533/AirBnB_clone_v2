a6ca2ea31c2a4b0eaf4b7e93b856cbad4f7f530b
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-20 18:27:55 +0200

    Ahmed Eid 2023/11/20 v0.00D

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
+from models.base_model import Base
 from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+import models
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """list of cities in state"""
+            Cities = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    Cities.append(city)
+            return Cities
+
