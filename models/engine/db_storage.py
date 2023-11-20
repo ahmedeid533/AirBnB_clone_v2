819ce370131f2eac2a31a6e446e0676a72f10b68
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-20 19:19:07 +0200

    Ahmed Eid 2023/11/20 v0.00F

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,1 +1,51 @@
 #!/usr/bin/python3
+"""DB storage"""
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
+
+class DBStorage:
+    """new engine"""
+
+    __engine = None
+    __session = None
+
+    
+    def __init__(self):
+        """start the engine"""
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True)
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """query on db by class name or select * """
+        if cls is None:
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
+        else:
+            if type(cls) == str:
+                cls = eval(cls)
+            Q_result = self.__session.query(cls)
+        return {"{}.{}".format(type(i).__name__, i.id): i for i in Q_result}
+
+
