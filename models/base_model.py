8a242d8fe604ad2ddce3467bc7c4f8928c5e9198
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-20 22:13:28 +0200

    Ahmed Eid 2023/11/20 v0.039

--- models/base_model.py
+++ models/base_model.py
@@ -42,6 +42,7 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        cls.pop("_sa_instance_state", None)
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
