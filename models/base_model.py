5393d9ed26c2075b0073cf336e4e22eae920c4b6
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-20 22:38:15 +0200

    Ahmed Eid 2023/11/20 v0.03D

--- models/base_model.py
+++ models/base_model.py
@@ -21,11 +21,9 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         elif 'id' not in kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -50,6 +48,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
