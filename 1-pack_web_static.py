3dbef68f4ea761c3f8a2a4ef79fb37f77ec7dd14
Emil dawood <edawood997@gmail.com>
2023-12-06 20:39:18 +0200

    d

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""store data(archives) in specfic folder"""
-from fabric import task
+"""genrate.tgz file"""
+
+from fabric.api import local, task
 from datetime import datetime
-import os
 
 
 @task
-def do_pack(c):
-    """do_pack funcation"""
-    source_folder = 'web_static'
-    target_folder = 'versions'
-    c.run(f'mkdir -p {target_folder}')
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_name = f'web_static_{timestamp}.tgz'
-    archive_path = os.path.join(target_folder, archive_name)
-    result = c.run(f'tar -cvzf {archive_path} {source_folder}')
-    if result.ok:
-        return archive_path
-    else:
+def do_pack():
+    """archive web_static"""
+    try:
+        time_now = datetime.now().strftime('%Y%m%d%H%M%S')
+        nameOfFile = 'web_static_{}.tgz web_static'.format(time_now)
+        local("mkdir -p versions")
+        local("tar -cvzf versions/{}".format(nameOfFile))
+        return "versions/"
+    except Exception:
         return None
