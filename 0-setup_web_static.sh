6d6c39cc10468af9006ef45d3d9bc90ec0bc07e8
Emil dawood <edawood997@gmail.com>
2023-12-06 20:20:04 +0200

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
-# create some folders and files to connect by nginx
-sudo apt-get -y update
-sudo apt-get install -y nginx
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo 'this is solve for task number 0 in airbnb V_2' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
-sudo -sh "echo 'server  {
-    listen 80
-    listen 80[::] dafualt
-    server_name realemildawood.tech
+# Prepare your web servers
+
+# install nginx
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo sh -c 'echo "this is Emad"> /data/web_static/releases/test/index.html'
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sh -c 'echo "server {
+    listen 80;
+    listen [::]:80 default_server;
+    server_name realemildawood.tech;
     location /hbnb_static {
         alias /data/web_static/current/;
     }
-}' > /etc/nginx/sites-available/default"
-sudo nginx -s reload
+}" > /etc/nginx/sites-available/default'
+sudo service nginx restart
