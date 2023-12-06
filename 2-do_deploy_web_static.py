32ae2af368e19b373206a60b38ddc4b3d1f66c0b
Emil dawood <edawood997@gmail.com>
2023-12-06 21:05:29 +0200

    third

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 "depoly by fabric"
-from MySQLdb import Connection
 from fabric.api import *
 from fabric import task
 from datetime import datetime
@@ -22,31 +21,29 @@ def do_pack():
 
 @task
 def do_deploy(archive_path):
-    """Deploy web_static to servers"""
-    env = {
-        "hosts": ['35.153.79.242', '52.201.164.137'],
-        "user": "your_username"  # Replace with your actual username
-    }
+    """deploy web_static to servers"""
+    env.hosts = ['35.153.79.242', '52.201.164.137']
     if not os.path.exists(archive_path):
         return False
-
     try:
-        for host in env["hosts"]:
-            with Connection(host=host, user=env["user"]) as conn:
-                filename = os.path.basename(archive_path).split('.')[0]
-                remote_tmp = f'/tmp/{filename}.tgz'
-                remote_release = f'/data/web_static/releases/{filename}/'
-
-                conn.put(archive_path, remote_tmp)
-                conn.run(f'mkdir -p {remote_release}')
-                conn.run(f'tar -xzf {remote_tmp} -C {remote_release}')
-                conn.run(f'rm {remote_tmp}')
-                conn.run(f'mv {remote_release}web_static/* {
-                    remote_release}')
-                conn.run(f'rm -rf {remote_release}web_static')
-                conn.run(f'rm -rf /data/web_static/current')
-                conn.run(f'ln -s {remote_release} /data/web_static/current')
-                print(f'New version deployed to {host}!')
+        for host in env.hosts:
+            env.host_string = host
+            web_static = "/data/web_static"
+            filename = archive_path.split('/')[-1]
+            update_filename = filename.split('.')[0]
+            put(archive_path, '/tmp/')
+            run(f'mkdir -p {web_static}/releases/{update_filename}/')
+            run(f'tar -xzf /tmp/{update_filename}.tgz -C \
+                {web_static}/releases/{update_filename}/')
+            run(f'rm /tmp/{update_filename}.tgz')
+            run(f'mv {web_static}/releases/{update_filename}/web_static/* \
+                {web_static}/releases/{update_filename}/')
+            run(
+                f'rm -rf {web_static}/releases/{update_filename}/web_static')
+            run(f'rm -rf {web_static}/current')
+            run(f'ln -s {web_static}/releases/{update_filename}/ \
+                {web_static}/current')
+            print('New version deployed!')
 
         return True
     except Exception:
