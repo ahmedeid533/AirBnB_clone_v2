4f30a3e724c4c2f1b376767745b3421e2c73bb4d
Emil dawood <edawood997@gmail.com>
2023-12-06 20:14:55 +0200

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,12 +7,12 @@ mkdir -p /data/web_static/releases/test/
 echo 'this is solve for task number 0 in airbnb V_2' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown ubuntu:ubuntu /data/
-echo 'server  {
+sudo -sh "echo 'server  {
     listen 80
     listen 80[::] dafualt
     server_name realemildawood.tech
     location /hbnb_static {
         alias /data/web_static/current/;
     }
-}' > /etc/nginx/sites-available/default
+}' > /etc/nginx/sites-available/default"
 sudo nginx -s reload
