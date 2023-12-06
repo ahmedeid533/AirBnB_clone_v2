e05d39c6464a77c6547b70e3b75035d600744c8f
Emil dawood <edawood997@gmail.com>
2023-12-06 21:35:39 +0200

    s

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,53 +1,52 @@
 #!/usr/bin/python3
-"""depoly by fabric"""
+"""genrate.tgz file"""
+
 from fabric.api import *
-from fabric import task
 from datetime import datetime
 import os
 
 
-env.hosts = ['54.89.57.165', '18.207.142.135']
-env.user = "ubuntu"
-
-
 @task
-def do_pack(c):
-    source_folder = 'web_static'
-    target_folder = 'versions'
-    c.run(f'mkdir -p {target_folder}')
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_name = f'web_static_{timestamp}.tgz'
-    archive_path = os.path.join(target_folder, archive_name)
-    result = c.run(f'tar -cvzf {archive_path} {source_folder}')
-    if result.ok:
-        return archive_path
-    else:
+def do_pack():
+    """archive web_static"""
+    try:
+        time_now = datetime.now().strftime('%Y%m%d%H%M%S')
+        nameOfFile = 'web_static_{}.tgz web_static'.format(time_now)
+        local("mkdir -p versions")
+        local("tar -cvzf versions/{}".format(nameOfFile))
+        return "versions/"
+    except Exception:
         return None
 
 
 @task
 def do_deploy(archive_path):
-    """fuck fabric depoly by command"""
+    """deploy function"""
+    env.hosts = ['54.89.57.165', '18.207.142.135']
     if not os.path.exists(archive_path):
-        return (False)
+        return False
     try:
-        li = archive_path.split('/')
-        file_name = li[-1]
-        path_of_releases = "/data/web_static/releases/{}/".format(
-            file_name[:-4])
-        path_of_tmp = "/tmp/{}".format(file_name)
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_of_releases))
-        run("tar -xzf {} -C {}".format(path_of_tmp, path_of_releases))
-        run("rm {}".format(path_of_tmp))
-        run("mv {}web_static/* {}".format(path_of_releases, path_of_releases))
-        run("rm -rf {}web_static".format(path_of_releases))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(path_of_releases))
-        print("New version deployed!")
-        return (True)
-    except Exception:
-        return (False)
+        for host in env.hosts:
+            env.host_string = host
+            filename = archive_path.split('/')[-1]
+            up_filename = filename.split('.')[0]
+            put(archive_path, '/tmp/')
+            run(f'mkdir -p /data/web_static/releases/{up_filename}/')
+            run(f'tar -xzf /tmp/{up_filename}.tgz -C \
+                /data/web_static/releases/{up_filename}/')
+            run(f'rm /tmp/{up_filename}.tgz')
+            run(f'mv /data/web_static/releases/{up_filename}/web_static/* \
+                /data/web_static/releases/{up_filename}/')
+            run(
+                f'rm -rf /data/web_static/releases/{up_filename}/web_static')
+            run(f'rm -rf /data/web_static/current')
+            run(f'ln -s /data/web_static/releases/{up_filename}/ \
+                /data/web_static/current')
+            print('New version deployed!')
+
+        return True
+    except Exception as err:
+        return False
 
 
 @task
