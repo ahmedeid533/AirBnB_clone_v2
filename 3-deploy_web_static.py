a081ce463cbe042d0768ab5c80c391976954e444
Emil dawood <edawood997@gmail.com>
2023-12-06 22:07:37 +0200

    last one is

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -25,33 +25,13 @@ def do_deploy(archive_path):
     env.hosts = ['54.89.57.165', '18.207.142.135']
     if not os.path.exists(archive_path):
         return False
-    try:
-        for host in env.hosts:
-            env.host_string = host
-            filename = archive_path.split('/')[-1]
-            up_filename = filename.split('.')[0]
-            put(archive_path, '/tmp/')
-            run(f'mkdir -p /data/web_static/releases/{up_filename}/')
-            run(f'tar -xzf /tmp/{up_filename}.tgz -C \
-                /data/web_static/releases/{up_filename}/')
-            run(f'rm /tmp/{up_filename}.tgz')
-            run(f'mv /data/web_static/releases/{up_filename}/web_static/* \
-                /data/web_static/releases/{up_filename}/')
-            run(
-                f'rm -rf /data/web_static/releases/{up_filename}/web_static')
-            run(f'rm -rf /data/web_static/current')
-            run(f'ln -s /data/web_static/releases/{up_filename}/ \
-                /data/web_static/current')
-            print('New version deployed!')
-
+    else:
         return True
-    except Exception as err:
-        return False
 
 
 @task
 def deploy():
-    """full deployment"""
+    """deploy function"""
     try:
         path = do_pack()
         if path is None:
