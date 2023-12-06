aa2e5efd3256f50abb71d1fa646a1066e7924174
Emil dawood <edawood997@gmail.com>
2023-12-06 20:40:43 +0200

    third task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,17 +11,15 @@ env.user = "ubuntu"
 
 
 @task
-def do_pack(c):
-    source_folder = 'web_static'
-    target_folder = 'versions'
-    c.run(f'mkdir -p {target_folder}')
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_name = f'web_static_{timestamp}.tgz'
-    archive_path = os.path.join(target_folder, archive_name)
-    result = c.run(f'tar -cvzf {archive_path} {source_folder}')
-    if result.ok:
-        return archive_path
-    else:
+def do_pack():
+    """archive web_static"""
+    try:
+        time_now = datetime.now().strftime('%Y%m%d%H%M%S')
+        nameOfFile = 'web_static_{}.tgz web_static'.format(time_now)
+        local("mkdir -p versions")
+        local("tar -cvzf versions/{}".format(nameOfFile))
+        return "versions/"
+    except Exception:
         return None
 
 
