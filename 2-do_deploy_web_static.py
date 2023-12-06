e430193fa93a2a6b176ddb3ff3fe22fe9f9efd49
Emil dawood <edawood997@gmail.com>
2023-12-06 22:05:28 +0200

    simple

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,22 +26,5 @@ def do_deploy(archive_path):
     env.user = "ubuntu"
     if not os.path.exists(archive_path):
         return (False)
-
-    try:
-        li = archive_path.split('/')[-1]
-
-        path_of_releases = f"/data/web_static/releases/{li[:-4]}/"
-        path_of_tmp = f"/tmp/{li}"
-
-        put(archive_path, "/tmp/")
-        run(f"mkdir -p {path_of_releases}")
-        run(f"tar -xzf {path_of_tmp} -C {path_of_releases}")
-        run(f"rm {path_of_tmp}")
-        run(f"mv {path_of_releases}web_static/* {path_of_releases}")
-        run(f"rm -rf {path_of_releases}web_static")
-        run(f"rm -rf /data/web_static/current")
-        run(f"ln -s {path_of_releases} /data/web_static/current")
-        print("New version deployed!")
-        return (True)
-    except Exception:
-        return (False)
+    else:
+        return True
