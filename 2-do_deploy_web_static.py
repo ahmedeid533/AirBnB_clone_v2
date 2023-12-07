da92d34ba361dda5cd2f6636e33a70cea71eb52d
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-07 21:25:18 +0200

    airBnbCloneV2 v0.03

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,9 +10,9 @@ def do_pack():
     """web_static"""
     try:
         time = datetime.now().strftime('%Y%m%d%H%M%S')
-        name = 'web_static_{}.tgz web_static'.format(time)
+        name_we = 'web_static_{}.tgz web_static'.format(time)
         local("mkdir -p versions")
-        local("tar -cvzf versions/{}".format(name))
+        local("tar -cvzf versions/{}".format(name_we))
         return "versions/"
     except Exception:
         return None
@@ -23,9 +23,33 @@ def do_deploy(archive_path):
     """deploy"""
     env.hosts = ['100.25.215.68', '100.25.147.204']
     env.user = "ubuntu"
-    if not os.path.exists(archive_path):
-        print("false")
-        return (False)
-    else:
-        print("true")
-        return True
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
