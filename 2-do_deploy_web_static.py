31bb0ab47b62d1005bc903140d623672060c70a4
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-07 20:02:48 +0200

    transfer

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
+
 """genrate.tgz file"""
 
-from fabric.api import *
+from fabric.api import local, task, env
 from datetime import datetime
 import os
-
 
 @task
 def do_pack():
@@ -22,7 +21,7 @@ def do_pack():
 @task
 def do_deploy(archive_path):
     """deploy function"""
-    env.hosts = ['54.89.57.165', '18.207.142.135']
+    env.hosts = ['100.25.215.68', '100.25.147.204']
     env.user = "ubuntu"
     if not os.path.exists(archive_path):
         return (False)
