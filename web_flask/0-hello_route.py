acc99d13198d7a0403b071491225c136efe9144b
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-28 09:37:20 +0200

    flask v0.23.12.28.03

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """ Displays 'Hello HBNB!' """
-    return 'Hello, HBNB!'
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
     """ Main Function """
