038ed12d7fb011f28283a0d0519132d7a3f90bb8
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-28 10:32:36 +0200

    flask v0.23.12.28,05.0

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask , render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Displays 'Hello HBNB!' """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays 'HBNB' """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Displays 'C <text>' """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """ Displays 'Python <text>' """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ Displays 'n is a number' """
+    return '{} is a number'.format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+	""" Displays 'n is a number' """
+	return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
