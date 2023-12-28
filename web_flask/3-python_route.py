eb0d2c4a931d9cc5153294b6e4a80276ef2edce8
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-28 09:51:41 +0200

    flask v0.23.12.28,03.0

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,13 @@ def c(text):
     """ Displays 'C <text>' """
     return 'C {}'.format(text.replace('_', ' '))
 
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+	""" Displays 'Python <text>' """
+	return 'Python {}'.format(text.replace('_', ' '))
+
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
