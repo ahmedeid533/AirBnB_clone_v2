0990d776806420649ef9197656f004c15356caab
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-28 09:35:49 +0200

    flask v0.23.12.28.02

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ Starts a Flask web application """
-from flask import Flask, request
+from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/', strict_slashes=False)
