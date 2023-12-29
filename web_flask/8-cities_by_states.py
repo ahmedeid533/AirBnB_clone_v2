5dc11fbfc16968fb3c7689c60e6ff2b3d76d0e6f
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-12-29 17:57:22 +0200

    flask v0.23.12.28,09.02

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,18 +12,11 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ Displays 'n is a number' """
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-	""" Displays 'n is a number' """
-	states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-	return render_template('8-cities_by_states.html', states=states)
+    """ Displays 'n is a number' """
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
 
 if __name__ == "__main__":
     """ Main Function """
